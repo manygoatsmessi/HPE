3342c030a0528ab3d44a226ba3a0aefc117f4524
space <kevin.feng4812@gmail.com>
2024-03-05 21:18:28 +1100

    HPE changes

--- animate.js
+++
@@ -1,8 +0,0 @@
-// // animated objects
-// const animate1 = document.querySelector('.monster');
-
-// // animations
-// const observer = new IntersectionObserver(entries => {
-//   animate1.classList.toggle( 'animation', entries[0].isIntersecting );
-// });
-// observer.observe( animate1 );
--- script.js
+++ script.js
@@ -6,66 +6,44 @@ window.onbeforeunload = function () {
     document.getElementById('screen-darkener').classList.add('darken');
 }
 
+const container = document.querySelector('.section-container');
+const items = document.querySelectorAll('.section');
 
-var prevScrollpos = window.pageYOffset;
+container.addEventListener('wheel', (event) => {
+  event.preventDefault();
+  const delta = event.deltaY;
 
-function displaywheel(e){
-  var evt=window.event || e //equalize event object
-  var currentScrollPos = window.pageYOffset;
-  var delta=evt.detail? evt.detail*(-120) : evt.wheelDelta //check for detail first so Opera uses that instead of wheelDelta
-  if (delta >= 0) {
-		document.getElementById("navigation-bar").style.opacity = "1";
-    document.getElementById("navigation-bar").style.visibility = 'visible';
-  }	else if (delta < 0) {
-		document.getElementById("navigation-bar").style.opacity = "0";
-    document.getElementById("navigation-bar").style.visibility = "hidden";
-  }
-  // if (delta > 1 && currentScrollPos == 0) {
-	// 	document.getElementById("home-header-section").style.opacity = "1";
-  //   document.getElementById("home-header-section").style.height = "100vh";
-  //   document.getElementById("body").style.overflow = 'hidden';
-  // }	else if (delta < 1) {
-	// 	document.getElementById("home-header-section").style.opacity = "0";
-  //   document.getElementById("home-header-section").style.height = "0px";
-  //   document.getElementById("body").style.overflow = 'visible';
-  // }
-  if (delta < 1) {
-		document.getElementById("home-header-section").style.opacity = "0";
-    document.getElementById("home-header-section").style.height = "0px";
-    document.getElementById("body").style.overflow = 'visible';
-  } else if (delta > 1 && currentScrollPos == 0) {
-    document.getElementById("home-header-section").style.opacity = "1";
-    document.getElementById("home-header-section").style.height = "100vh";
-    document.getElementById("body").style.overflow = 'hidden';
-  }
-  prevScrollpos = currentScrollPos;
-}
-
-var mousewheelevt=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
-
-if (document.attachEvent) //if IE (and Opera depending on user setting)
-  document.attachEvent("on"+mousewheelevt, displaywheel)
-else if (document.addEventListener) //WC3 browsers
-  document.addEventListener(mousewheelevt, displaywheel, false)
+  container.scrollBy({
+    top: delta,
+    behavior: 'smooth'
+  });
+});
 
 
-window.onscroll = function() {
-	var currentScrollPos = window.pageYOffset;
-  var eighthscreenheight = (screen.height)/8
-	var quarterscreenheight = (screen.height)/4
-	var halfscreenheight = (screen.height)/2
 
-  if (prevScrollpos >= currentScrollPos && currentScrollPos == 0) {
-		document.getElementById("home-header-section").style.opacity = "1";
-    document.getElementById("home-header-section").style.height = "100vh";
-    document.getElementById("body").style.overflow = 'hidden';
-  }
 
-  prevScrollpos = currentScrollPos;
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('transition');
+      return;
+    }
+    entry.target.classList.remove('transition');
+  });
+});
 
-  if (currentScrollPos == 0) {
-		document.getElementById("navigation-bar").style.backgroundColor = "rgba(0,0,0,0)";
-	} else {
-		document.getElementById("navigation-bar").style.backgroundColor = "rgba(0,0,0,1)";
-	}
-}
+// Get multiple elements instead of a single one using "querySelectorAll"
+const sideTopicUnderline = document.querySelectorAll('.side-topic-underline');
+const sideTopic = document.querySelectorAll('.side-topic');
+const paragraphSection = document.querySelectorAll('.paragraph-section');
+const table = document.querySelectorAll('.table');
+const statNumber = document.querySelectorAll('.stat-number');
+const stat = document.querySelectorAll('.stat');
+
+// Loop over the elements and add each one to the observer
+sideTopicUnderline.forEach((element) => observer.observe(element));
+sideTopic.forEach((element) => observer.observe(element));
+paragraphSection.forEach((element) => observer.observe(element));
+table.forEach((element) => observer.observe(element));
+statNumber.forEach((element) => observer.observe(element));
+stat.forEach((element) => observer.observe(element));
