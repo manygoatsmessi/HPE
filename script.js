5ae222e70c57534a57d8dc2300730deb7d08a6d5
space <kevin.feng4812@gmail.com>
2024-03-07 20:44:38 +1100

    8:44pm, 7/03/2024 - First Commit, Basic Structure

--- script.js
+++ script.js
@@ -39,6 +39,12 @@ const paragraphSection = document.querySelectorAll('.paragraph-section');
 const table = document.querySelectorAll('.table');
 const statNumber = document.querySelectorAll('.stat-number');
 const stat = document.querySelectorAll('.stat');
+const quote = document.querySelectorAll('.quote');
+const category = document.querySelectorAll('.category');
+const statPageStat = document.querySelectorAll('.stat-page-stat');
+const statPage = document.querySelectorAll('.statistic-page');
+// const pie = document.querySelectorAll('.pie');
+
 
 // Loop over the elements and add each one to the observer
 sideTopicUnderline.forEach((element) => observer.observe(element));
@@ -47,3 +53,31 @@ paragraphSection.forEach((element) => observer.observe(element));
 table.forEach((element) => observer.observe(element));
 statNumber.forEach((element) => observer.observe(element));
 stat.forEach((element) => observer.observe(element));
+quote.forEach((element) => observer.observe(element));
+category.forEach((element) => observer.observe(element));
+statPageStat.forEach((element) => observer.observe(element));
+statPage.forEach((element) => observer.observe(element));
+
+
+
+
+
+
+
+
+
+
+
+const observer1 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animation');
+      return;
+    }
+    entry.target.classList.remove('animation');
+  });
+});
+
+
+const pie = document.querySelectorAll('.pie');
+pie.forEach((element) => observer1.observe(element));
